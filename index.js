e770051b2763666c7b76f963ccb6110c53398f37
Eugene Pempel <epempel@sailthru.com>
2020-02-26 21:09:40 +1300

    Add a draft implementation of the /events route handler

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
 const express = require('express')
+const { WebClient } = require('@slack/web-api')
+
+const port = process.env.PORT
+const slackToken = process.env.SLACK_TOKEN
+const users = JSON.parse(Buffer.from(process.env.USERS, 'base64').toString('ascii'))
+
 const app = express()
-const port = process.env.PORT || 8000
+const slackClient = new WebClient(slackToken)
 
 app.use(express.json())
 
-app.get('/', (_, res) => {
-  res.send('Hello World!')
+app.post('/messages', async (req, res, next) => {
+  try {
+    await slackClient.chat.postMessage({
+      text: 'Hi there!',
+      channel: 'channel',
+      as_user: true
+    });
+    res.status(204).send()
+  } catch(error) {
+    return next(error)
+  }
 })
 
 app.post('/events', (req, res) => {
-  console.log(req.body)
-  res.status(204)
+  const event = req.body
+  switch (event.action) {
+    case 'review_requested': {
+      const { url, number, title } = event.pull_request
+      const userName = event.pull_request.user.login
+      const reviewerNames = event.requested_reviewers
+      console.log(url, number, title, userName, reviewerNames)
+    }
+    default: {
+      console.log(`EVENT NOT HANDLED: ${event.action}`)
+    }
+  }
+  res.status(204).send()
+})
+
+// Error handler should be placed after all other middlewares
+app.use((err, req, res, next) => {
+  error = { code: err.code, message: err.message }
+  console.error(`ERROR: ${JSON.stringify(error)}`)
+  res.status(500).send(error)
 })
 
 app.listen(port, () => {
