674ba6c69099503e81a71725e1b3797fcb88dba6
Eugene Pempel <epempel@sailthru.com>
2020-02-26 21:59:14 +1300

    Handle the `review_requested` event via the /events handler

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const asyncHandler = require('express-async-handler')
 const { WebClient } = require('@slack/web-api')
 
 const port = process.env.PORT
@@ -10,27 +11,24 @@ const slackClient = new WebClient(slackToken)
 
 app.use(express.json())
 
-app.post('/messages', async (req, res, next) => {
-  try {
-    await slackClient.chat.postMessage({
-      text: 'Hi there!',
-      channel: 'channel',
-      as_user: true
-    });
-    res.status(204).send()
-  } catch(error) {
-    return next(error)
-  }
-})
-
-app.post('/events', (req, res) => {
+app.post('/events', asyncHandler(async (req, res) => {
   const event = req.body
   switch (event.action) {
     case 'review_requested': {
-      const { url, number, title } = event.pull_request
-      const userName = event.pull_request.user.login
-      const reviewerName = event.requested_reviewer
-      console.log(url, number, title, userName, reviewerName)
+      const pull = event.pull_request
+      const userName = pull.user.login
+      const user = users.find(u => u.gname === userName)
+      if (!user) {
+        throw new Error(`User with the github name '${userName}' not found`)
+      }
+      const reviewerName = event.requested_reviewer.login
+      const reviewer = users.find(u => u.gname === reviewerName)
+      if (!reviewer) {
+        throw new Error(`User with the github name '${reviewerName}' not found`)
+      }
+      const text = `${user.name} requested your review on <${pull.url}|PR#${pull.number}>: ${pull.title}`
+      const channel = reviewer.sid
+      await slackClient.chat.postMessage({ text, channel, as_user: true });
       break
     }
     default: {
@@ -38,7 +36,7 @@ app.post('/events', (req, res) => {
     }
   }
   res.status(204).send()
-})
+}))
 
 // Error handler should be placed after all other middlewares
 app.use((err, req, res, next) => {
